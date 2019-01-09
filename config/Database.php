e18294d89db0cd529ee460e9a347103dc27558fc
Excellent <excellent.nomad@gmail.com>
2019-01-10 01:23:27 +0600

    added charset utf8 to pdo

--- config/Database.php
+++ config/Database.php
@@ -12,8 +12,13 @@
       $this->conn = null;
 
       try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, 
+                              $this->username, $this->password,
+                              array(
+                                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+                              ));
+        // $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
       }
